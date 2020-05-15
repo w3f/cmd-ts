9335a35b1c86c204b3f2fd43008bfd179282bffe
Federico Gimenez <federico.gimenez@gmail.com>
2020-05-15 18:08:12 +0200

    add stdio field to CmdOptions

--- src/types.ts
+++ src/types.ts
@@ -2,10 +2,13 @@ interface EnvVar {
     [key: string]: string;
 }
 
+type Stdio = 'pipe' | 'inherit' | 'ignore';
+
 export interface CmdOptions {
     detached?: boolean;
     matcher?: RegExp;
     verbose?: boolean;
     env?: EnvVar;
     shell?: boolean;
+    stdio?: Stdio;
 }
